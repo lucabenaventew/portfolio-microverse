ca222032eb1844bec15034d6b7dff99b96eebcd4
lucabenaventew <cacachuuuluca@gmail.com>
2022-06-12 22:34:43 -0500

    Update Scripts file

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,12 @@
-const hamburger = document.querySelector(".hamburger");
-const menu = document.querySelector(".nav_links_container");
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.nav_links_container');
 
-hamburger.addEventListener("click", function openMenu() {
-  this.classList.toggle("is-active");
-  menu.classList.toggle("show");
+hamburger.addEventListener('click', function openMenu() {
+  this.classList.toggle('is-active');
+  menu.classList.toggle('show');
 });
 
-menu.addEventListener("click", function closeMenu() {
-  this.classList.toggle("show");
-  hamburger.classList.toggle("is-active");
+menu.addEventListener('click', function closeMenu() {
+  this.classList.toggle('show');
+  hamburger.classList.toggle('is-active');
 });
