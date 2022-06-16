51c56e4227c485ba56317d7cd361121f1c264720
lucabenaventew <cacachuuuluca@gmail.com>
2022-06-15 22:35:30 -0500

    Update js files

---
+++ Scripts/form_validation.js
@@ -0,0 +1,35 @@
+const data = document.querySelectorAll('#Form-form input, #Form-form textarea');
+const error = document.querySelectorAll('#Form-form small');
+const form = document.querySelector('#Form-form');
+
+if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+  data[2].value = 'Sent from my mobile phone';
+}
+
+form.addEventListener('submit', (e) => {
+  for (let i = 0; i < data.length; i += 1) {
+    if (data[i].value === '') {
+      e.preventDefault();
+      data[i].classList.add('error');
+      error[i].textContent = `Fill ${data[i].getAttribute('placeholder')} field first`;
+      error[3].textContent = `Fill ${data[i].getAttribute('placeholder')} field first`;
+      break;
+    }
+    if (data[i].value !== '') {
+      data[i].classList.remove('error');
+      error[i].textContent = '';
+      error[3].textContent = '';
+    }
+  }
+
+  if (data[1].value.toLowerCase() !== data[1].value) {
+    e.preventDefault();
+    data[1].classList.add('error');
+    error[1].textContent = 'Email cant contain upercase';
+    error[3].textContent = 'Email cant contain upercase';
+    e.preventDefault();
+  } else {
+    error[1].textContent = '';
+    data[1].classList.remove('error');
+  }
+});
